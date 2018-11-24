b7b9e6fcebdc6a064ccc13d4de62fdeb07509e05
Einar Ingebrigtsen <einar@dolittle.com>
2018-11-24 08:53:27 +0100

    Initial start for the boilerplate

---
+++ Content/Core/Startup.cs
@@ -0,0 +1,20 @@
+
+namespace Core
+{
+    public partial class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+
+        public void ConfigureContainer(ContainerBuilder containerBuilder)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+        }
+
+    }
+}
